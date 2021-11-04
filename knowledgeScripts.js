10e845ac56325a43acff16f8ff277180c0d5f4c7
dominicfoxy <dj.jonoman@gmail.com>
2021-11-04 15:13:54 -0500

    7 files modified

--- knowledgeScripts.js
+++ knowledgeScripts.js
@@ -2,7 +2,7 @@ function couldYouNot() {
   document.getElementById('none').innerHTML =
     "Could you NOT decompile my code please? It's cheating and not the intended solution";
 }
-
+// Checks to see if you've figured out where the road leads
 function verify01() {
   if (
     document.getElementById('first').value === 'parsley' &&
@@ -10,6 +10,7 @@ function verify01() {
     document.getElementById('third').value === 'rosemary' &&
     document.getElementById('fourth').value === 'thyme'
   ) {
-    document.getElementById('main').innerHTML = '<h1>Well Done!</h1><a href="#">The next step</a>';
+    document.getElementById('main').innerHTML =
+      '<h1>Well Done!</h1><a href="#">Did you know that phones see things that desktops can\'t?</a>';
   }
 }
