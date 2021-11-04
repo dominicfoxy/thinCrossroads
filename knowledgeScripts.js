bbacdec761219b119f2b9ebe9c3277a380836c90
dominicfoxy <dj.jonoman@gmail.com>
2021-11-04 11:57:23 -0500

    knowledge scripts now work

--- knowledgeScripts.js
+++ knowledgeScripts.js
@@ -1,9 +1,15 @@
 function couldYouNot() {
-  document.getElementById("none").innerHTML = "Could you NOT decompile my code please? It's cheating and not the intended solution";
+  document.getElementById('none').innerHTML =
+    "Could you NOT decompile my code please? It's cheating and not the intended solution";
 }
 
-function verify() {
-  if (document.getElementById("first").value === "parsley") {
-    document.getElementById("main").innerHTML = "<h1>Well Done!</h1>";
+function verify01() {
+  if (
+    document.getElementById('first').value === 'parsley' &&
+    document.getElementById('second').value === 'sage' &&
+    document.getElementById('third').value === 'rosemary' &&
+    document.getElementById('fourth').value === 'thyme'
+  ) {
+    document.getElementById('main').innerHTML = '<h1>Well Done!</h1><a href="#">The next step</a>';
   }
 }
